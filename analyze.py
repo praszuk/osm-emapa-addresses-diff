cef3a747283ab4d8eb527cf4b646f3a968b65252
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-10-01 20:39:46 +0200

    Add docstr

--- analyze.py
+++ analyze.py
@@ -5,6 +5,9 @@ from address import Address, OsmAddress
 
 
 def addr_tags_distribution(addresses: List[OsmAddress]) -> Counter:
+    """
+    :return: tags counter distribution of usage addr* tags + source:addr
+    """
     tags = Counter()
     for addr in addresses:
         for key in addr.all_obj_tags.keys():
@@ -15,6 +18,9 @@ def addr_tags_distribution(addresses: List[OsmAddress]) -> Counter:
 
 
 def addr_type_distribution(addresses: List[OsmAddress]) -> Counter:
+    """
+    :return: osm type counter distribution usage
+    """
     osm_type = Counter()
     for addr in addresses:
         osm_type[addr.osm_type] += 1
@@ -23,6 +29,9 @@ def addr_type_distribution(addresses: List[OsmAddress]) -> Counter:
 
 
 def addr_duplicates(osm_addresses: List[OsmAddress]) -> List[List[OsmAddress]]:
+    """
+    :return: duplicated addresses (checks by city street and housenumber)
+    """
     duplicated_osm_addr: Dict[str, List[OsmAddress]] = dict()
 
     for osm_addr in osm_addresses:
@@ -41,6 +50,9 @@ def addr_missing(
     osm_addresses: List[OsmAddress],
     emapa_addresess: List[Address]
 ) -> List[Address]:
+    """
+    :return: missing addresses in the OSM from emapa_addresses
+    """
     all_osm_min_addr: Set[str] = set()
     for osm_addr in osm_addresses:
         osm_min_addr = f'{osm_addr.city}' \
