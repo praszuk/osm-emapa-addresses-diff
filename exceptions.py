a66228604670c42ac8a1734d653ee3e9bed8cac1
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-01-07 14:22:45 +0100

    Remove unused exception

--- exceptions.py
+++ exceptions.py
@@ -1,11 +1,4 @@
-class TerytNotFound(Exception):
-    """
-    Raises when Teryt from params is not matched with external table data
-    """
-    pass
-
-
-class EmapaServiceNotFound(Exception):
+class ServiceNotFound(Exception):
     """
     Raises when e.g commune use different addresses system (e.g. geoportal)
     """
