3ea0b3f87df754b9847827bc05f8404ca7d45dca
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-01-28 12:35:04 +0100

    Add downloader of capabilities from punktadresowe

--- utils/emapa_downloader.py
+++ utils/emapa_downloader.py
@@ -1,6 +1,8 @@
 import requests
+
 from config import gettext as _, logger
 from exceptions import ServiceNotFound
+from typing import Optional
 
 
 PUNKTYADRESOWE_URL = 'https://www.punktyadresowe.pl/' \
@@ -10,6 +12,9 @@ PUNKTYADRESOWE_URL = 'https://www.punktyadresowe.pl/' \
                      '&SRSNAME=urn:ogc:def:crs:EPSG::4326' \
                      '&typenames=ms:punkty_adresowe' \
                      '&version=2.0.0'
+
+PUNKTYADRESOWE_SOURCE_EMAPA_URL = 'https://www.punktyadresowe.pl/' \
+                                  'cgi-bin/emuia/<teryt>'
 
 
 def download_emapa_gml(teryt: str, gml_filename: str) -> None:
@@ -29,3 +34,20 @@ def download_emapa_gml(teryt: str, gml_filename: str) -> None:
 
     with open(gml_filename, 'wb') as f:
         f.write(response.content)
+
+
+def download_punktyadresowe_metadata(teryt: str) -> Optional[str]:
+    """
+    :param teryt: commune (gmina) id number (6 characters)
+    :raises ServiceNotFound, IOError:
+    :return XML/GML metadata – capabilities content from punktyadresowe WFS
+    """
+
+    logger.info(_('Downloading punktyadresowe metadata...'))
+    url = PUNKTYADRESOWE_SOURCE_EMAPA_URL.replace('<teryt>', teryt)
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise ServiceNotFound
+
+    return response.text
